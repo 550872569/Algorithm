948dfe052e23a96192beb67acf05cb261b47ce31
YY <550872569@qq.com>
2018-07-01 16:32:04 +0800

    add func guessThePrice()

--- Algorithm/1.2.2看商品猜价格/main.m
+++ Algorithm/1.2.2看商品猜价格/main.m
@@ -9,6 +9,38 @@
 #import <Foundation/Foundation.h>
 #include "stdio.h"
 
+
+/**
+ 需求：
+    猜测商品价格 500 - 1000  取整10
+    二分法
+    1. 先设置一个价格
+    2. 每次猜完主持人告诉 用户高了或者低了
+    3. 直到猜对为止，输出猜测次数
+ */
+void guessThePrice() {
+    int oldPrice = 0, price = 0, i = 0;
+    printf("\n请输入预设价格500-1000:");
+    scanf("%d",&oldPrice);
+    while (oldPrice!=price) {
+        i++;
+        printf("\n参与者:");
+        scanf("%d",&price);
+        if (oldPrice>price) {
+            printf("主持人:低了");
+        } else if (oldPrice<price) {
+            printf("主持人:高了");
+        } else {
+            printf("恭喜你猜对了!答案是:%d,您猜了%d次。",price,i);
+        }
+    }
+
+}
+
+
+
+
+
 void guessPrice() {
     int oldPrice = 0, price = 0, i = 0;
     printf("请设置商品价格！500˜1000\n");
@@ -32,7 +64,7 @@ void guessPrice() {
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        guessPrice();
+        guessThePrice();
     }
     return 0;
 }
